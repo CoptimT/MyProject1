55ce5c1e93a7875e675cfac854a8a9809302f1d5
zhangxiangwei <zhangxiangwei@yixia.com>
2017-06-06 14:54:50 +0800

    渠道推广项目

--- channel_spread/dist/hive/create/dwv_yzb_channel_spread.sql
+++ channel_spread/dist/hive/create/dwv_yzb_channel_spread.sql
@@ -1,14 +1,15 @@
 CREATE EXTERNAL TABLE `dwv_yzb.dwv_yzb_channel_spread`(
+  day string,
   app string,
-  eventtime string,
+  event_time string,
   idfa string,
-  spreadname string,
+  spread_name string,
   ip string,
-  devicetype string,
-  appkey string,
-  clickip string,
-  clicktime string,
-  adnetname string,
+  device_type string,
+  app_key string,
+  click_ip string,
+  click_time string,
+  adnet_name string,
   tdid string,
   ua string)
 PARTITIONED BY (`dt` string,`hour` string)
