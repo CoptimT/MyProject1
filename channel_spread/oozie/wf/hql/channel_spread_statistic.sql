8f0af57ac1e33f0600aeab871a99375d0c90749b
zhangxiangwei <zhangxiangwei@yixia.com>
2017-06-07 17:20:49 +0800

    渠道推广项目

--- channel_spread/oozie/wf/hql/channel_spread_statistic.sql
+++ channel_spread/oozie/wf/hql/channel_spread_statistic.sql
@@ -1,6 +1,7 @@
 -- 1.结果统计
 
 CREATE TABLE IF NOT EXISTS dwv_yzb.dwv_yzb_channel_spread_statistic(
+  day          string comment '日期',
   app          string comment 'app',
   spread_name  string comment '渠道',
   today_add    bigint comment '今天的新增',
@@ -13,7 +14,7 @@ CREATE TABLE IF NOT EXISTS dwv_yzb.dwv_yzb_channel_spread_statistic(
 
 
 insert overwrite table dwv_yzb.dwv_yzb_channel_spread_statistic partition (day='${day}')
-select app,spread_name,sum(today_add) today_add,sum(today_update) today_update,
+select '${day}',app,spread_name,sum(today_add) today_add,sum(today_update) today_update,
        sum(yesterday_add) yesterday_add,sum(yesterday_update) yesterday_update,
        concat(nvl(unix_timestamp(),0),'000'),concat(nvl(unix_timestamp(),0),'000') from (
 
@@ -33,5 +34,5 @@ union all
   select app,spread_name,0 today_add,0 today_update,0 yesterday_add,count(distinct idfa) yesterday_update
   from temp_yzb.tmp_channel_spread_mid_${day} where cnt>1 and last_date='${yesterday}' group by app,spread_name
 
-) tb app,spread_name;
+) tb group by app,spread_name;
 
