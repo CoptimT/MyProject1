efdedcf418af5a1829068a47bb751321505910c0
zhangxiangwei <zhangxiangwei@yixia.com>
2017-06-09 11:53:09 +0800

    渠道推广项目

--- channel_spread/oozie/wf/hql/channel_spread_prepare.sql
+++ channel_spread/oozie/wf/hql/channel_spread_prepare.sql
@@ -13,7 +13,6 @@
 -- 1.任务定时需在凌晨1点后
 -- 2.先将IDFA去重再关联查询
 
-
 -- 1.数据准备
 -- 1.1 查询当天去重IDFA记录
 
@@ -21,9 +20,9 @@
 drop table if exists temp_yzb.tmp_channel_spread_${day};
 -- 1.1.2 执行查询
 create table temp_yzb.tmp_channel_spread_${day} as
-select app,idfa,spread_name from dwv_yzb.dwv_yzb_channel_spread where (dt='${day}' or (dt='${tomorrow}' and hour='00')) and day='${day}'
-and spread_name is not null and idfa!='00000000-0000-0000-0000-000000000000' group by app,idfa,spread_name;
-
+select app,spread_name,idfa from dwv_yzb.dwv_yzb_channel_spread where (dt='${day}' or (dt='${tomorrow}' and hour='00')) and day='${day}'
+and app is not null and spread_name is not null and idfa!='00000000-0000-0000-0000-000000000000' and idfa is not null
+group by app,spread_name,idfa;
 
 -- 1.2 关联查询IDFA出现次数和最新出现时间
 
@@ -31,9 +30,11 @@ and spread_name is not null and idfa!='00000000-0000-0000-0000-000000000000' gro
 drop table if exists temp_yzb.tmp_channel_spread_mid_${day};
 -- 1.2.2 执行查询
 create table temp_yzb.tmp_channel_spread_mid_${day} as
-select t.app,t.spread_name,t.idfa,count(1) cnt,coalesce(from_unixtime(max(t.createtime),'yyyyMMdd'),'') last_date
+select t.app,t.spread_name,t.idfa,coalesce(from_unixtime(min(t.createtime),'yyyyMMdd'),'') first_date,coalesce(from_unixtime(max(t.createtime),'yyyyMMdd'),'') last_date
 from (
-    select b.app,b.spread_name,b.idfa,a.createtime from dwv_yzb.dwv_yzb_xk_member_device_faid a
-    left join temp_yzb.tmp_channel_spread_${day} b on(a.faid=b.idfa)
+    select b.app,b.spread_name,b.idfa,a.createtime
+    from (
+        select faid,createtime from dwv_yzb.dwv_yzb_xk_member_device_faid where createtime<unix_timestamp('${tomorrow}','yyyyMMdd')
+    ) a join temp_yzb.tmp_channel_spread_${day} b
+    on(a.faid=b.idfa)
 ) t group by t.app,t.spread_name,t.idfa;
-
--- channel_spread/oozie/wf/hql/channel_spread_statistic.sql
+++ channel_spread/oozie/wf/hql/channel_spread_statistic.sql
@@ -15,25 +15,26 @@ stored as textfile;
 
 
 insert overwrite table dwv_yzb.dwv_yzb_channel_spread_statistic partition (dt='${day}')
-select '${day}',app,spread_name,sum(today_add) today_add,sum(today_update) today_update,
+select from_unixtime(unix_timestamp('${day}', 'yyyyMMdd'),'yyyy-MM-dd') day,app,spread_name,
+       sum(today_add) today_add,sum(today_update) today_update,
        sum(yesterday_add) yesterday_add,sum(yesterday_update) yesterday_update,
-       concat(nvl(unix_timestamp(),0),'000'),concat(nvl(unix_timestamp(),0),'000') from (
+       nvl(unix_timestamp(),0),nvl(unix_timestamp(),0) from (
 
-  --a.最新出现时间为当天且只出现了一次的idfa数量，则统计为今天的新增
+  --a.最早出现时间为当天的idfa数量，则统计为今天的新增
   select app,spread_name,count(distinct idfa) today_add,0 today_update,0 yesterday_add,0 yesterday_update
-  from temp_yzb.tmp_channel_spread_mid_${day} where cnt=1 and last_date='${day}' group by app,spread_name
+  from temp_yzb.tmp_channel_spread_mid_${day} where first_date='${day}' group by app,spread_name
 union all
-  --b.最新出现时间为当天且出现次数>1的idfa数量，则统计为今天的升级
+  --b.最新出现时间为今天且今天之前也有出现的idfa数量，则统计为今天的升级
   select app,spread_name,0 today_add,count(distinct idfa) today_update,0 yesterday_add,0 yesterday_update
-  from temp_yzb.tmp_channel_spread_mid_${day} where cnt>1 and last_date='${day}' group by app,spread_name
+  from temp_yzb.tmp_channel_spread_mid_${day} where last_date='${day}' and last_date!=first_date group by app,spread_name
 union all
-  --c.最新出现时间为昨天且只出现了一次的idfa数量，则统计为昨天的新增
+  --c.最早出现时间为昨天且今天没有出现的idfa数量，则统计为昨天的新增
   select app,spread_name,0 today_add,0 today_update,count(distinct idfa) yesterday_add,0 yesterday_update
-  from temp_yzb.tmp_channel_spread_mid_${day} where cnt=1 and last_date='${yesterday}' group by app,spread_name
+  from temp_yzb.tmp_channel_spread_mid_${day} where first_date='${yesterday}' and first_date=last_date group by app,spread_name
 union all
-  --d.最新出现的时间为昨天且出现次数>1的idfa数量，则统计为昨天的升级
+  --d.最新出现的时间为昨天且昨天之前也出现的idfa数量，则统计为昨天的升级
   select app,spread_name,0 today_add,0 today_update,0 yesterday_add,count(distinct idfa) yesterday_update
-  from temp_yzb.tmp_channel_spread_mid_${day} where cnt>1 and last_date='${yesterday}' group by app,spread_name
+  from temp_yzb.tmp_channel_spread_mid_${day} where last_date='${yesterday}' and last_date!=first_date group by app,spread_name
 
 ) tb group by app,spread_name;
 
